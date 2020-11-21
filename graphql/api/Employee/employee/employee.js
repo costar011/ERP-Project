56284f40e415f3cdf166aaab2194543501be9636
YERIM <dnaer456@gmail.com>
2020-11-21 22:02:29 +0900

    empl

--- graphql/api/Employee/employee/employee.js
+++ graphql/api/Employee/employee/employee.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import Employee from "../../../model/Employee";
 
 export default {
@@ -28,8 +27,39 @@ export default {
           birth,
         });
 
-        console.log(result);
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    deleteEmployee: async (_, args) => {
+      const { id } = args;
 
+      try {
+        const result = await Employee.deleteOne({ _id: id });
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    updateEmployee: async (_, args) => {
+      const { id, name, attitude, team } = args;
+
+      try {
+        const result = await Employee.updateOne(
+          { _id: id },
+          {
+            $set: {
+              name,
+              attitude,
+              team,
+            },
+          }
+        );
+        console.log(result);
         return true;
       } catch (e) {
         console.log(e);
